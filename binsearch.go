6874b839831ed3cd9743614261b178212ce99647
schmittydog <sdunn@corevera.com>
2025-01-20 12:18:27 -0800

    fixed bsisin right bound

--- binsearch.go
+++ binsearch.go
@@ -6,7 +6,7 @@ func BSIsIn(arr []int, value int) bool {
 	if len(arr) == 0 {
 		return false
 	}
-	l, r := 0, len(arr)
+	l, r := 0, len(arr) - 1
 	for r > l {
 		m := (r + l) / 2
 		if arr[m] < value {
