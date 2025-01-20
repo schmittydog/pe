3f163bc6bcf22c67c98d2abe843b736b9cf7cbed
schmittydog <sdunn@corevera.com>
2025-01-20 12:40:21 -0800

    bsleft finalized

--- binsearch.go
+++ binsearch.go
@@ -6,11 +6,11 @@ func BSIsIn(arr []int, value int) bool {
 	if len(arr) == 0 {
 		return false
 	}
-	l, r := 0, len(arr) - 1
+	l, r := 0, len(arr)-1
 	for r > l {
 		m := (r + l) / 2
 		if arr[m] < value {
-			l = m + 1	
+			l = m + 1
 		} else {
 			r = m
 		}
@@ -18,38 +18,36 @@ func BSIsIn(arr []int, value int) bool {
 	return arr[l] == value
 }
 
-
 // BSRight bisect right
 func BSRight(arr []int, value int) int {
-        if len(arr) == 0 {
-                return 0
-        }
-        l, r := 0, len(arr)
-        for r > l {
-                m := (r + l) / 2
-                if arr[m] >= value {
-                        r = m - 1
-                } else {
-                        l = m
-                }
-        }
-        return l
+	if len(arr) == 0 {
+		return 0
+	}
+	l, r := 0, len(arr)
+	for r > l {
+		m := (r + l) / 2
+		if arr[m] >= value {
+			r = m - 1
+		} else {
+			l = m
+		}
+	}
+	return l
 }
 
 // BSLeft bisect left
 func BSLeft(arr []int, value int) int {
-        if len(arr) == 0 {
-                return 0
-        }
-        l, r := 0, len(arr)-1
-        for r > l {
-                m := (r + l) / 2
-                if arr[m] < value {
-                        l = m + 1
-                } else {
-                        r = m
-                }
-        }
-        return l
+	if len(arr) == 0 {
+		return 0
+	}
+	l, r := 0, len(arr)
+	for r > l {
+		m := (r + l) / 2
+		if arr[m] < value {
+			l = m + 1
+		} else {
+			r = m
+		}
+	}
+	return l
 }
-
