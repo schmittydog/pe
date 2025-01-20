a8113f35291c7206bc2e6c841ed1462f6f9fe83f
schmittydog <sdunn@corevera.com>
2025-01-20 10:29:50 -0800

    changed val to value

--- binsearch.go
+++ binsearch.go
@@ -8,13 +8,13 @@ func BSIsIn(arr []int, value int) bool {
 	l, r := 0, len(arr) - 1
 	for r > l + 1 {
 		m := l + (r-l)/2
-		if arr[m] == val {
+		if arr[m] == value {
 			return true
-		} else if arr[m] > val {
+		} else if arr[m] > value {
 			r = m
 		} else {
 			l = m
 		}
 	}
-	return arr[l] == val || arr[r] == val
+	return arr[l] == value || arr[r] == value
 }
